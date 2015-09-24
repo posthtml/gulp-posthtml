3ba1df4f0494efe2e20ad212202de20955b488d7
Ivan Voischev <voischev.ivan@ya.ru>
2015-09-24 13:16:11 +0300

    release 1.0.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# gulp-posthtml
+[![npm version](https://badge.fury.io/js/gulp-posthtml.svg)](http://badge.fury.io/js/gulp-posthtml)
+
+## Usage
+
+```js
+
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+var posthtml = require('posthtml');
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+
+// Consts
+var PLUGIN_NAME = 'gulp-posthtml';
+
+module.exports = function(plugins, options) {
+
+    if (!plugins) {
+        throw new PluginError(PLUGIN_NAME, 'No plugin?');
+    }
+
+    return through.obj(function(chunk, enc, cb) {
+        if (chunk.isNull()) {
+          // return empty file
+          return cb(null, chunk);
+        }
+        posthtml([].concat(plugins))
+            .process(String(chunk.contents), options)
+            .then(function(result) {
+                chunk.contents = new Buffer(result.html);
+                cb(null, chunk);
+            });
+   });
+};
